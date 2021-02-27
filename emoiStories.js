c83e6beb60744466c7a3b185fcc5635ae2f8fea9
Flower Aviles <itsfloweraviles@gmail.com>
2021-02-27 12:37:24 -0500

    Trying to find a solution in JS.

---
+++ emoiStories.js
@@ -0,0 +1,22 @@
+const emojis = [
+    '😀','😆','😅','🤣','🙃','😍','😡','🥶','😱','👻',
+    '👾','😻','💋','👅','🧵','👠','🥾','👑','🎓','🐔',
+    '🐧','🦄','🦇','🌲','🎄','🌊','🍓','🍍','🥯','🌭',
+    '🍕','🍣','🥡','🤸','🎯','🧨','🔮','💉','🦠','❤️',
+    '🧡','💛','💚','💙','💜','☢️','🚫','💯','🔱','🎊'
+]
+
+const generateStory = () => {
+    let input = Number(document.querySelector("#story-length-input").value);
+    let p = document.getElementById("emoji-story");
+
+    const str = input;
+    // let output = [emojis.Math.floor or something like that]; 
+    // let emojis = the amount the of input.value 
+    // emojis needs to printed randomly 
+    // trying to find the right code to make output print random emojis! 
+    // p.textContext = output; this code will give me the output 
+
+
+
+}
